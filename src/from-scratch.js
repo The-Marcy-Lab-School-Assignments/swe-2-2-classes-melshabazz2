a6acdd781afc70300645e887799c130562e12f2a
melshabazz2 <melshabazz2@gmail.com>
2025-11-06 17:53:08 -0500

    completed from scratch

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,20 +1,115 @@
 class Rectangle {
-
+  constructor(length, width) {
+    this.length = length
+    this.width = width
+  }
+  getArea() {
+    return this.length * this.width
+  }
+  getPerimeter() {
+    return this.length + this.length + this.width + this.width
+  }
+  isSquare() {
+    if (this.length === this.width) {
+      return true
+    } else {
+      return false
+    }
+  }
 }
 
 class Vehicle {
-
+  passengers = [];
+  constructor(type, capacity, color) {
+    this.type = type
+    this.capacity = capacity
+    color ? this.color = color : this.color = "black"
+  }
+  paint(color) {
+    return this.color = color
+  }
+  addPassenger(passenger) {
+    if (this.passengers.length === this.capacity) {
+      return -1
+    }
+    this.passengers.push(passenger)
+    return this.passengers.length
+  }
 }
 
 class PasswordManager {
+  #password
+  constructor(password) {
+    this.#password = password
+
+  }
+  checkPassword(attempt) {
+    if (attempt === this.#password) {
+      return true
+    }
+    return false
+  }
+  setPassword(oldPassword, newPasssword) {
+    if (oldPassword === this.#password) {
+      this.#password = newPasssword
+      return true
+    }
+    return false
+  }
 
 }
 
 class TodoList {
+  #list = []
+  constructor(title) {
+    this.title = title
+  }
+  addItem(description) {
+    this.#list.push(description)
+    return this.#list.length
+  }
+  removeItem(description) {
+    if (this.#list.includes(description)) {
+      // .splice(index, 0 === add 1 === replace, "wasdwas")
+      this.#list.splice(this.#list.indexOf(description), 1)
+      return description
+    }
+    return null
+  }
+  getItems() {
+    return [...this.#list]
+  }
 
 }
 
 class BankAccount {
+  #balance
+  static #totalBalance = 0
+  constructor(firstName, lastName, balance) {
+    this.firstName = firstName
+    this.lastName = lastName
+    balance ? this.#balance = balance : this.#balance = 0
+    BankAccount.#totalBalance += this.#balance
+  }
+  deposit(amount) {
+    this.#balance += amount
+    BankAccount.#totalBalance += amount
+    return `Your balance is $${this.#balance.toFixed(2)}`
+  }
+  withdraw(amount) {
+    if (amount > this.#balance) {
+      return `You do not have enough funds.`
+    }
+    this.#balance -= amount
+    BankAccount.#totalBalance -= amount
+    return `Your balance is $${this.#balance.toFixed(2)}.`
+  }
+  static getTotalHoldings() {
+    return BankAccount.#totalBalance
+  }
+  showBalance() {
+    return `Your balance is $${this.#balance.toFixed(2)}`
+  }
 
 }
 
--- src/playground.js
+++ src/playground.js
@@ -1,1 +1,30 @@
-console.log('Put your code here!');
+class Rectangle {
+    constructor(length, width) {
+        this.length = length
+        this.width = width
+    }
+    getArea() {
+        return this.length * this.width
+    }
+    getPerimeter() {
+        this.length * 2 + this.width * 2
+    }
+    isSquare() {
+        if (Rectangle === 'square') {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+const myShape = new Rectangle(10, 5); // length = 10, width = 5
+console.log(myShape.getArea()); // 50
+console.log(myShape.getPerimeter()); // 30 (10 * 2 + 5 * 2)
+console.log(myShape.isSquare()); // false
+
+// length and width are public properties so we can mutate them
+myShape.width = 3;
+myShape.length = 3;
+console.log(myShape.getArea()); // 9
+console.log(myShape.getPerimeter()); // 12
+console.log(myShape.isSquare()); // true
